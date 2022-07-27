8c6981826efc77f81ab60ac4ca72bc3d1af550c1
JackNoordhuis <jackjnoordhuis@gmail.com>
2022-07-27 15:13:54 +1000

    setup-pmmp-phpstan-env action: load rules with autoload script

--- config/autoload_rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-/**
- * Simple autoload script for the PHPStan rules we ship the action with.
- */
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . "rules/DisallowEnumComparisonRule.php";
